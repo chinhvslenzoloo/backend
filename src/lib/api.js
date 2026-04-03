ba34edd300f2a7166f080bd1f460b55a27537f69
chinhvslenzoloo <chinhvslenzoloo@gmail.com>
2026-04-03 11:11:31 +0800

    po

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  "https://frontend-production-fc88.up.railway.app/";
 const client = axios.create({
   baseURL,
   withCredentials: true,
